dd15c6f1aed1c76256994109b0c980f04afd9ff5
IDforDeveloping <jwjs3219@gmail.com>
2021-12-08 10:56:31 +0900

    first commit

---
+++ src/main/java/com/ajt/be/ajt/AjtApplication.java
@@ -0,0 +1,13 @@
+package com.ajt.be.ajt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AjtApplication.class, args);
+	}
+
+}
